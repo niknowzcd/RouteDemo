e1e4cbfaf79d95c9c22bdb6cd9c87d3d9aa10a71
duliangyu <1922966878@qq.com>
2018-07-15 23:20:34 +0800

    update

---
+++ route_annotation/src/main/java/com/dly/route_annotation/AutoWired.java
@@ -0,0 +1,17 @@
+package com.dly.route_annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by a01 on 2018/7/15.
+ */
+
+@Target({ElementType.FIELD})
+@Retention(RetentionPolicy.CLASS)
+public @interface AutoWired {
+
+    String name() default "";
+}
